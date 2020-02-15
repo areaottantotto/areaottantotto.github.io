3e7e48eacc21076d27d636d56ee2c51ca70ad0f0
Andrea Andreetta <60930759+areaottantotto@users.noreply.github.com>
2020-02-15 10:05:12 +0100

    major update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function aafw_open() {
+    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+  }
+  function aafw_close() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+  }
