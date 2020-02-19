7efe0b1c53ce8cf0e25161aee272ca60da1bd1e5
areaottantotto <60930759+areaottantotto@users.noreply.github.com>
2020-02-19 16:01:48 +0100

    up footer and responsive photo

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 function aafw_open() {
-    document.getElementById("main").style.marginLeft = "40%";
-    document.getElementById("mySidebar").style.width = "40%";
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
+  document.getElementById("main").style.marginLeft = "50%";
+  document.getElementById("mySidebar").style.width = "50%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
   }
+
   function aafw_close() {
-    document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
   }
